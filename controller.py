79d0503e86a95855a9a8c7d81aff074efad905bf
Baticaly <batuhan@batuhanergun.com>
2021-07-28 00:03:59 +0300

    Initial Commit

---
+++ controller.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+import socket
+import pygame
+import json
+import time
+import os
+
+load_dotenv()
+HOST = os.environ.get("HOST")
+PORT = int(os.environ.get("PORT", 3000))
+
+controllerInit, socketInit = 0, 0
+
+clock = pygame.time.Clock()
+
+while True:
+
+    # Controller initialization
+    if controllerInit == 0:
+        try:
+            pygame.init()
+            pygame.joystick.init()
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+        except:
+            controllerInit = 0
+            print("Joystick initialization failed.")
+            time.sleep(1)
+        else:
+            #Print joystick name & ID
+            controllerInit = 1
+
+    # Socket initialization
+    if socketInit == 0:
+        try:
+            tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcpSocket.connect((HOST, PORT))
+        except:
+            socketInit = 0
+            print("Socket connection failed.")
+            time.sleep(1)
+        else:
+            socketInit = 1
+
+    # Data read from controller axes
+    pygame.event.get()
+    aResult, bResult = [], []
+    axes = joystick.get_numaxes()
+    for i in range(axes):
+        axis = joystick.get_axis(i)
+        aResult.append('{:>6.3f}'.format(axis))
+
+    # Data read from controller buttons
+    buttons = joystick.get_numbuttons()
+    for i in range(buttons):
+        button = joystick.get_button(i)
+        bResult.append('{:>2}'.format(button))
+
+    # JSON encode for socket communication
+    data = json.dumps({"axis": aResult, "buttons": bResult})
+
+    # Package transfer & Error handling
+    try:
+        tcpSocket.sendall(data.encode())
+        statusMessage = tcpSocket.recv(1024)
+    except:
+        socketInit = 0
+        print("Connection refused.")
+
+    # Debug
+    #print(statusMessage)
+    
+    # PyGame tickrate
+    clock.tick(20)
---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+import socket
+import json
+import time
+import os
+
+load_dotenv()
+HOST = os.environ.get("HOST")
+PORT = int(os.environ.get("PORT", 3000))
+
+socketInit = 0
+controllerData =  '{}'
+
+def failsafe():
+    # Failsafe parameters & orientation fix
+    print("FAILSAFE")
+    controllerData = '{}'
+
+def packageHandle(data):
+    try:
+        data = json.loads(data)
+        axis = data.get("axis")
+        buttons = data.get("buttons")
+    except:
+        failsafe()
+        conn.close()
+        print("Unexpected package, connection closed.")
+        socketInit = 0
+        return 0, 0
+    else:
+        return axis, buttons
+
+while True:
+
+    # Socket initialization
+    if socketInit == 0:
+        try:
+            serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serversocket.bind((HOST, PORT))
+            serversocket.listen()
+            conn, addr = serversocket.accept()
+        except:
+            socketInit = 0
+            print("No connection.")
+            time.sleep(1)
+        else:
+            print('Connected by', addr)
+            socketInit = 1
+
+    # Package transfer & Error handling & JSON parse
+    if socketInit == 1:
+        try:
+            data = conn.recv(1024)
+            conn.sendall(data)
+            controllerData = data
+        except:
+            socketInit = 0
+            print("Connection refused.")
+        else:
+            axis, buttons = packageHandle(controllerData)
+
+    print(axis)
+            
+            
+
